bd245baa3acd0b02c7bde7c39900d4604a7e8a5c
NHarraz <harraz22hatem@gmail.com>
2021-11-03 19:52:25 +0200

    Delete API

--- routes/api/flights.js
+++ routes/api/flights.js
@@ -87,4 +87,10 @@ router.put('/search', (req,res)=>{
 });
 
 
+// delete flight
+router.delete('/:_id',(req,res) => {
+    Flight.findByIdAndRemove(req.params._id,req.body).then(flight=>res.json({msg:'flight entry deleted successfully'}))
+    .catch(err=>res.status(404).json({error:'No such a flight'}))
+})
+
 module.exports = router
