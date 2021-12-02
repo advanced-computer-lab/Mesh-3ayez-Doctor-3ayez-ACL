7bac2d03773015b0cc38fd444e043d8fab73209b
AbdallahElsayed73 <abdallah.arafa@student.guc.edu.eg>
2021-12-02 19:19:29 +0200

    updated the schema of the reservation model and implemented the cancel reservation API without sending an email to the user yet

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,30 @@ const { Query } = require('mongoose');
 const mongoose = require('mongoose');
 const Flight = require('../../src/Models/Flight');
 const User = require('../../src/Models/User');
+const Reservation = require('../../src/Models/Reservation')
+const FlightSeat = require('../../src/Models/FlightSeat');
 
 const router = express.Router()
+
+// user cancel a reservation
+
+router.delete('/reservation/:user_id/:reservation_id', async(req,res)=>{
+    
+    const user_id = req.params.user_id;
+    const reservation_id = req.params.reservation_id
+    const reservation = await Reservation.find({'_id':reservation_id, 'user_id':user_id});
+    if(reservation.length>0)
+    {
+        await FlightSeat.updateMany({'reservation_id' : reservation_id}, {'reservation_id':null});
+        await Reservation.findByIdAndDelete(reservation_id);
+        res.json({msg:"deleted successfully"});
+
+    }
+    else
+        res.status(404).json({msg: 'no such reservations for this specific user'});
+    
+})
+
 
 
 
