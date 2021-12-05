813f5896e78660cf925478330f5793190f3884e8
NHarraz <harraz22hatem@gmail.com>
2021-12-05 10:43:56 +0200

    password added

--- src/client/src/components/UserProfile.js
+++ src/client/src/components/UserProfile.js
@@ -2,13 +2,19 @@ import FOG from 'vanta/dist/vanta.fog.min'
 import axios from 'axios';
 import React, { useState, useEffect, useRef } from 'react'
 import * as THREE from "three";
-import * as React2 from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import BasicAlert from "./BasicAlert";
 import Stack from '@mui/material/Stack';
 import { Hidden } from '@mui/material';
+import IconButton from "@material-ui/core/IconButton";
+import InputLabel from "@material-ui/core/InputLabel";
+import Visibility from "@material-ui/icons/Visibility";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormControl from '@mui/material/FormControl';
 
 export default function UserProfile(probs) {
     const id = probs.id;
@@ -34,17 +40,52 @@ export default function UserProfile(probs) {
             if (vantaEffect) vantaEffect.destroy()
         }
     }, [vantaEffect])
+    
+    const [row,setRow] = useState({
+        id: probs.id,
+        first_name: probs.first_name,
+        last_name: probs.last_name,
+        username:probs.lastname,
+        password: probs.password,
+        passport:probs.passport,
+        email:probs.email
+      });
     const [username, setUsername] = useState(probs.username)
     const [FirstName, setFirstName] = useState(probs.FirstName)
     const [LastName, setLastName] = useState(probs.LastName)
     const [email, setEmail] = useState(probs.email);
     const [Passport, setPassport] = useState(probs.Passport)
+    const [Password, setPassword] = useState(probs.Password)
+    const [Password1, setPassword1] = useState({
+        password: "",
+        showPassword: false,
+    })
+    const [Password2, setPassword2] = useState({
+        password: "",
+        showPassword: false,
+    })
+    const handleClickShowPassword1 = () => {
+        setPassword1({ ...Password1, showPassword: !Password1.showPassword });
+    };
+    const handleClickShowPassword2 = () => {
+        setPassword2({ ...Password2, showPassword: !Password2.showPassword });
+    };
+    const handlePasswordChange1 = (prop) => (event) => {
+        setPassword1({ ...Password1, [prop]: event.target.value });
+    };
+        
+    const handleMouseDownPassword1 = (event) => {
+        event.preventDefault();
+    };
+    const handlePasswordChange2 = (prop) => (event) => {
+        setPassword2({ ...Password2, [prop]: event.target.value });
+    };
+        
+    const handleMouseDownPassword2 = (event) => {
+        event.preventDefault();
+    };
 
-    const [errorusername, setErrorusername] = useState("");
-    const [errorFirstName, setErrorFirstName] = useState("");
-    const [errorLastName, setErrorLastName] = useState("");
-    const [erroremail, setErroremail] = useState("");
-    const [errorPassport, setErrorPassport] = useState("");
+    const [errorPassword2, setErrorPassword2] = useState("");
 
     const [alert, setAlert] = useState(false);
     const [succ, setSucc] = useState(false);
@@ -52,26 +93,48 @@ export default function UserProfile(probs) {
 
     const onSubmit = function (e) {
         e.preventDefault()
+        setUsername(username?username:row.username);
+        setFirstName(FirstName?FirstName:row.first_name);
+        setLastName(LastName?LastName:row.last_name);
+        setEmail(email?email:row.email);
+        setPassport(Passport?Passport:row.passport);
+
+        
+        var f=false;
+
+        // if (!username)
+        //     setErrorusername("This field is required");
+        // if (!FirstName)
+        //     setErrorFirstName("This field is required");
+        // if (!LastName)
+        //     setErrorLastName("This field is required");
+        // if (!email)
+        //     setErroremail("This field is required");
+        // if (!Passport)
+        //     setErrorPassport("This field is required");
+        if (Password1){
+            if (!Password2)
+                setErrorPassword2("This field is required");
+            else if(Password1!=Password2){
+                setErrorPassword2("Password doesn't match");
+            }
+            else{
+                setPassword(Password1);
+            }
+           
+        }else{
+            setPassword(row.password);
+        }
         const data = {
             "username": username,
             "FirstName": FirstName,
             "LastName": LastName,
             "email": email,
             "Passport": Passport,
+            "Password":Password
+            
         }
-
-        if (!username)
-            setErrorusername("This field is required");
-        if (!FirstName)
-            setErrorFirstName("This field is required");
-        if (!LastName)
-            setErrorLastName("This field is required");
-        if (!email)
-            setErroremail("This field is required");
-        if (!Passport)
-            setErrorPassport("This field is required");
-
-        if (username && FirstName && LastName && email && Passport) {
+        if (username || FirstName || LastName || email || Passport ||Password) {
             axios.put("http://localhost:8000/api/users/" + id, data, { "Content-Type": "application/json" })
                 .then(result => {
                     setSucc(result.data.success);
@@ -94,7 +157,7 @@ export default function UserProfile(probs) {
             <div className='background' ref={myRef} style = {{     paddingTop: '1px',
     marginTop: '-1px' }}>
             <div className = "center"  >
-                <div id = 'box' style={{  margin: "auto", marginTop: "100px", backgroundColor: "#142F43", width: "640px", height: "80px", textAlign: "center", boxShadow: "px #999999" }}>
+                <div id = 'box' style={{  margin: "auto", marginTop: "70px", backgroundColor: "#142F43", width: "640px", height: "80px", textAlign: "center", boxShadow: "px #999999" }}>
                     <h1 style={{ color: "whitesmoke", padding: "20px" }}>Edit Profile</h1>
                 </div>
 
@@ -114,81 +177,140 @@ export default function UserProfile(probs) {
 
                     >
                         <TextField
-                            required
+                            
                             onChange={function (e) {
                                 setUsername(e.target.value);
-                                setErrorusername(e.target.value ? "" : "This field is required");
                             }}
                             value={username}
                             id="outlined-required"
                             label="Username"
                             type="String"
-                            error={errorusername ? true : false}
-                            helperText={errorusername}
                         />
 
                         <TextField
-                            required
+                            
                             id="outlined-required"
                             label="First Name"
                             onChange={function (e) {
                                 setFirstName(e.target.value);
-                                setErrorFirstName(e.target.value ? "" : "This field is required");
                             }
                             }
                             value={FirstName}
                             type="String"
-                            error={errorFirstName ? true : false}
-                            helperText={errorFirstName}
 
                         />
                         <TextField
-                            required
                             id="outlined-required"
                             label="Last Name"
                             onChange={function (e) {
                                 setLastName(e.target.value);
-                                setErrorLastName(e.target.value ? "" : "This field is required");
                             }
                             }
                             value={LastName}
                             type="String"
-                            error={errorLastName ? true : false}
-                            helperText={errorLastName}
-
                         />
                         <TextField
-                            required
                             id="outlined-required"
                             label="Email"
                             onChange={function (e) {
                                 setEmail(e.target.value);
-                                setErroremail(e.target.value ? "" : "This field is required")
                             }}
                             value={email}
                             type="String"
-                            error={erroremail ? true : false}
-                            helperText={erroremail}
                         />
+                        
+                        <InputLabel htmlFor="outlined-adornment-password">Change Password</InputLabel>
+                        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" label="Password">
+                        <InputLabel htmlFor="outlined-adornment-password">New Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                label="Password"
+                                type={Password1.showPassword ? 'text' : 'password'}
+                                onChange={handlePasswordChange1('password')}
+                                endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword1}
+                                onMouseDown={handleMouseDownPassword1}
+                                edge="end"
+                                >
+                                {Password1.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                            }
+                        />
+                        </FormControl>
+                        <FormControl  sx={{ m: 1, width: '25ch' }} variant="outlined" >
+                            <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                label="Password"
+                                type={Password2.showPassword ? 'text' : 'password'}
+                                onChange={handlePasswordChange2('password')}
+                                endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword2}
+                                onMouseDown={handleMouseDownPassword2}
+                                edge="end"
+                                >
+                                {Password2.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+
+                            }
+                        />
+                        </FormControl>
+
+                        
                         <TextField
                             required
                             id="outlined-required"
                             label="Passport"
                             onChange={function (e) {
                                 setPassport(e.target.value);
-                                setErrorPassport(e.target.value ? "" : "This field is required")
                             }}
                             value={Passport}
                             type="Number"
-                            error={errorPassport ? true : false}
-                            helperText={errorPassport}
                         />
+                         {/* <TextField
+                            id="outlined-required"
+                            label="New Password"
+                            type={Password1.showPassword ? "text" : "password"}
+                            onChange={handlePasswordChange1("password")}
+                            endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                onClick={handleClickShowPassword1}
+                                onMouseDown={handleMouseDownPassword1}
 
+                                >
+                                {Password1.showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                                </InputAdornment>
+                            }
+                        /> */}
+                         {/* <TextField
+                            id="outlined-required"
+                            type="password"
+                            label="Confirm new password"
+                            onChange={function (e) {
+                                setPassword2(e.target.value);
+                            }
+                            }
+                            error={errorPassword2 ? true : false}
+                            helperText={errorPassword2}
+
+                        /> */}
 
                         <br />
+                        <br />
                         <Stack direction="row" spacing={1} >
-                            <Button style={{ textAlign: "center", marginLeft: "auto" }} variant="contained" onClick={onSubmit} href="/">Submit </Button>
-                            <Button style={{ margin: "auto" }} variant="outlined" href="/">Back </Button>
+                            <Button style={{ textAlign: "center", marginLeft: "auto",height:"30px" }} variant="contained" onClick={onSubmit} href="/">Submit </Button>
+                            <Button style={{ margin: "auto",height:"30px" }} variant="outlined" href="/">Back </Button>
                         </Stack>
                         <br />
 			{alert && <BasicAlert severity={succ ? "success" : "error"} msg={msg} />}
