9cf45eadb6f37f9656532c696100a1ed73425ae3
AbdallahElsayed73 <abdallah.arafa@student.guc.edu.eg>
2021-12-24 21:47:40 +0200

    process env in login and signup apis

--- routes/api/login.js
+++ routes/api/login.js
@@ -6,16 +6,16 @@ const User = require('../../src/Models/User');
 var bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const dotenv=require('dotenv');
-dotenv.config({path:__dirname+'/.env'});
+dotenv.config({path:__dirname+'/../../config/.env'});
 
 const key= process.env.SECRETKEY;
-
 router.post("/", async (req, res) => {
   // Our login logic starts here
   try {
     // Get user input
 
     const { username, password } = req.body;
+    
 
     // Validate user input
     if (!(username )) {
